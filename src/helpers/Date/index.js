cc6f2bb72ad005ec423e5983e0da78a2e376b396
KarlBerod <karl.berod@gmail.com>
2024-05-02 09:26:23 +0200

    first commit

---
+++ src/helpers/Date/index.js
@@ -0,0 +1,16 @@
+export const MONTHS = {
+  0: "janvier",
+  1: "février",
+  2: "mars",
+  3: "avril",
+  4: "mai",
+  5: "juin",
+  6: "juillet",
+  7: "août",
+  8: "septembre",
+  9: "octobre",
+  10: "novembre",
+  11: "décembre",
+};
+// changes were maid to adapt the index of the months, index 0 did not exist and caused january month to never display
+export const getMonth = (date) => MONTHS[date.getMonth()];
